3063b2c5577584886e299db8354cc4ffbff577cb
augcolas <augus.colas@orange.fr>
2024-03-22 15:57:18 +0100

    hover effect on movies

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ import { useEffect } from "react";
 import { useAuth } from '../src/contexts/auth.context';
 import BurgerMenu from '../src/components/burger.menu.component';
 import { useTheme } from '@mui/material/styles';
-
 import styles from '../src/styles/index.module.css';
 export default function Index() {
 
@@ -22,6 +21,12 @@ export default function Index() {
     const { user, logout } = useAuth();
 
     const [movies, setMovies] = React.useState([]);
+
+    const optionsCursorTrueWithMargin = {
+        followCursor: true,
+        shiftX: 20,
+        shiftY: 0
+    };
 
     useEffect(() => {
         fetch('/api/movies')
@@ -58,28 +63,15 @@ export default function Index() {
                     </Box>
                     <Container>
                         <Grid container spacing={4}>
-                            {movies.map((movie) => (
+                            {movies.map((movie,index) => (
                                 <Grid item key={movie.id} xs={12} sm={6} md={4}>
                                     <Card>
                                         <CardActionArea>
                                             <Box className={styles.cardImage}
-                                                sx={{
-                                                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`,
-                                                }}
+                                                sx={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`,}}
                                             >
-                                                <Typography gutterBottom variant="h5" component="div"
-                                                sx={{
-                                                    position: 'absolute',
-                                                    bottom: 0,
-                                                    left: 0,
-                                                    right: 0,
-                                                    backgroundColor: 'rgba(0,0,0,0.5)',
-                                                    color: 'white',
-                                                    padding: 2,
-
-                                                }}
-                                                >
-                                                    {movie.title}
+                                                <Typography gutterBottom variant="h5" component="div" className={styles.cardTitle}>
+                                                    <span className={styles.hoverText}>{movie.title}</span>
                                                 </Typography>
                                             </Box>
                                         </CardActionArea>
